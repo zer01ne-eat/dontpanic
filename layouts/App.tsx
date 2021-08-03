f436a7c9a5c6a5208a7bb676d3cf0159519ac6a8
kreativemoon <mnyo@kakao.com>
2021-08-03 20:43:31 +0900

    change component signup to modal, add svg data as json file, add lottie

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,8 +1,19 @@
 import React, { useState, useCallback } from 'react';
 import loadable from '@loadable/component';
+import data from '../d.json'
+import Lottie from 'react-lottie';
 
 const NavBar = loadable(() => import('@layouts/navbar'));
-const SignUp = loadable(() => import('@components/signup'));
+const Modal = loadable(() => import('@components/modal'));
+
+const lottieOptions = {
+    animationData: data,   
+    loop: true,        
+    autoplay: true,   
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
 const App = () => {
     const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(true);
@@ -12,9 +23,15 @@ const App = () => {
       }, []);
 
     return (
-        <div><NavBar />
-    <SignUp show={showCreateWorkspaceModal} onCloseModal={onCloseModal}>sdfsdf</SignUp>
-    </div>
+        <div>
+            <NavBar />
+        	{/* <Lottie options={lottieOptions} /> */}
+            <Modal show={showCreateWorkspaceModal} onCloseModal={onCloseModal}>
+                캐릭터 선택
+                blahsdf
+                sdlkfjsd
+            </Modal>
+        </div>
     );
 };
 
