c3b2665c3d8d64d0b80f077acaac66cfcf3c3a18
kristine <77544317+kreativemoon@users.noreply.github.com>
2021-08-11 02:20:54 +0900

    navbar userinfo, menus, menu contents layout added

--- layouts/navbar/index.tsx
+++ layouts/navbar/index.tsx
@@ -1,7 +1,10 @@
 import React, { useCallback, useEffect, useState } from 'react';
 // import { Switch, Route } from "react-router";
-import { NavLink, Switch, Route } from "react-router-dom";
-import { LeftNavBar } from "./styles";
+import { NavLink, Switch, Route } from 'react-router-dom';
+import { LeftNavBar, UserInfo, Menus, WorkSpace } from './styles';
+import Chat from '@pages/NavBar/chat';
+import Goal from '@pages/NavBar/goal';
+import Profile from '@pages/NavBar/profile';
 
 const NavBar = () => {
     const [showNavBar, setShowNavBar] = useState(true);
@@ -12,9 +15,34 @@ const NavBar = () => {
     return (
         <div>
             <button onClick={onClickNavBar}>X</button>
-            {showNavBar && <LeftNavBar />}
+            {showNavBar && (
+                <LeftNavBar>
+                    <UserInfo>userinfo</UserInfo>
+                    <Menus>
+                        <NavLink exact to="/">
+                            home
+                        </NavLink>
+                        <NavLink exact to="/map/profile">
+                            profile
+                        </NavLink>
+                        <NavLink exact to="/map/goal">
+                            goal
+                        </NavLink>
+                        <NavLink exact to="/map/chat">
+                            chat
+                        </NavLink>
+                    </Menus>
+                    <WorkSpace>
+                        <Switch>
+                            <Route path="/map/profile" component={Profile} />
+                            <Route path="/map/goal" component={Goal} />
+                            <Route path="/map/chat" component={Chat} />
+                        </Switch>
+                    </WorkSpace>
+                </LeftNavBar>
+            )}
         </div>
     );
-}
+};
 
 export default NavBar;
