98ef4fb6fc7d5e297874d246f24b7ad946add2d5
kristine <77544317+kreativemoon@users.noreply.github.com>
2021-08-22 23:25:41 +0900

    back initial setting

---
+++ back/dist/0.2c102e15a53d0719055f.hot-update.js
@@ -0,0 +1,39 @@
+exports.id = 0;
+exports.ids = null;
+exports.modules = {
+
+/***/ 3:
+/***/ ((module, exports, __webpack_require__) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core_1 = __webpack_require__(4);
+const app_module_1 = __webpack_require__(5);
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Listening on port ${port}`);
+    if (true) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
+}
+bootstrap();
+
+
+/***/ })
+
+};
+exports.runtime =
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("5bec64ec1bf561245c65")
+/******/ })();
+/******/ 
+/******/ }
+;
