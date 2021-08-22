f4473890d30e315089c0995627789728c122f4c6
kristine <77544317+kreativemoon@users.noreply.github.com>
2021-08-22 20:40:34 +0900

    player components added in map comp

--- pages/Map/index.tsx
+++ pages/Map/index.tsx
@@ -1,11 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useEffect, useState } from 'react';
 import NavBar from '@layouts/navbar';
+import Player from "@components/character/player";
 
 const Map = () => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+    const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+    useEffect(() => {
+        if (!canvasRef.current) return;
+        const canvas = canvasRef.current;
+        setCanvas(canvas);
+        setCtx(canvas.getContext("2d"));
+    }, []);
+
     return (
         <>
             <NavBar />
-            <h1>MAP</h1>
+            <Player skin="m1" />
+            {/* <canvas ref={canvasRef} width={"100%"} height={"100%"}></canvas> */}
         </>
   );
 };
