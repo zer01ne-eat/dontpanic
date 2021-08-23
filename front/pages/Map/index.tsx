78423296db5cb2c1802c4b5bbba7641297b671fe
kristine <77544317+kreativemoon@users.noreply.github.com>
2021-08-24 02:11:31 +0900

    character added in Map

--- front/pages/Map/index.tsx
+++ front/pages/Map/index.tsx
@@ -2,11 +2,13 @@ import React, { FC, useState } from 'react';
 import NavBar from '@layouts/navbar';
 import { Link, Route } from 'react-router-dom';
 import CodeShare from '@pages/CodeShare';
+import Player from '@components/character/player';
 
 const Map = () => {
 
     return (
         <>
+            <Player skin="character-00" />
             <button><Link style={{"textDecoration": "none"}} to='/codeshare'>dd</Link></button>
             <Route path='/codeshare' component={CodeShare} />
         </>
