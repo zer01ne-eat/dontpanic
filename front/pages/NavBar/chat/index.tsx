a976645a031fd3cc48cce8cb86137fb5e3e8bea7
kreativemoon <mnyo@kakao.com>
2021-08-29 00:25:44 +0900

    modify chat components, modify navbar css styles

--- front/pages/NavBar/chat/index.tsx
+++ front/pages/NavBar/chat/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Chat = () => {
     return (
-        <div>
-            <h1>Chat</h1>
+        <div style={{width:"100%", height:"100%", padding: "20px", backgroundColor: "#1e1e1e"}}>
+            <div style={{border:"2px solid blue", height:"70%"}}>
+                
+            </div>
+            <div style={{height:"30%", border:"2px solid green",}}>
+                <textarea placeholder="Press Enter to send message." style={{color: "#fff",width:"100%", height:"100%", "border":"none", "backgroundColor": "#141414", position:"static"}}/>
+            </div>
         </div>
   );
 };
