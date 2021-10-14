7f35a8c5e30b482b6c15d7d9f561a2f3291ad9e4
kreativemoon <mnyo@kakao.com>
2021-10-15 00:54:41 +0900

    navbar profile graph gradient color applied

--- front/pages/NavBar/profile/index.tsx
+++ front/pages/NavBar/profile/index.tsx
@@ -1,51 +1,35 @@
-import React, { useRef, useState, useEffect, useCallback } from 'react';
+import React, { useRef, useState } from 'react';
 import { Contents, Skills, Oval } from './styles';
-import { Radar, Chart } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 import Projects from './projects';
 
 const Profile = () => {
     const [closeList, setCloseList] = useState(false);
     const [display, setDisplay] = useState('');
     
-    const chartRef = useRef<Chart | null>(null);
-
-
-    const projects = [
-        {name: 'project1', position: {x: 1550, y: 500}, type: 'html', list: [{name: 'Quiz1', marked: 100}, {name: 'Quiz2', marked: 10}]},
-        {name: 'project2', position: {x: 850, y: 700}, type: 'css', list: [{name: 'Quiz1', marked: 100}, {name: 'Quiz2', marked: 10}]},
-        {name: 'project3', position: {x: 1300, y: 1000}, type: 'js', list: [{name: 'Quiz1', marked: 100}, {name: 'Quiz2', marked: 10}]}
-    ]
-    const canvas = document.getElementsByTagName('canvas')[0] as HTMLCanvasElement
-    console.log(canvas);
-    // const ctx = canvas!.getContext('2d');
-    // const gradient = ctx!.createLinearGradient(20,0, 220,0);
-    // gradient.addColorStop(0, 'green');
-    // gradient.addColorStop(.5, 'cyan');
-    // gradient.addColorStop(1, 'green');
-    const data = {
-        labels: [
-            'HTML',
-            'CSS',
-            'Javascript',
-        ],
-        datasets: [{
-            data: [80, 70, 80],
-            backgroundColor: '#20f08b',
-            
-        }]
-    };
-    return (
-        <Contents>
-            <Skills>
-                <div className="title">SKILLS</div>
-                <div className="description">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum turpis sed pretium sodales.
-                </div>
-                <Radar ref={chartRef}
-                    data={data}
-                    width={10}
-                    height={5}
-                    options={{ 
+    const ref = useRef<Chart | null>(null);
+    const skillChart = (canvas:HTMLCanvasElement | null) => {
+        const ctx = canvas?.getContext('2d');
+        let gradient = ctx?.createLinearGradient(0.000, 0, 0, 150.000);
+        gradient?.addColorStop(0.035, 'rgba(234, 25, 78, 1.000)');
+        gradient?.addColorStop(0.225, 'rgba(0, 8, 249, 1.000)');
+        gradient?.addColorStop(0.983, 'rgba(42, 255, 0, 1.000)');
+        if (ctx) {
+            ref.current = new Chart(ctx, {
+                type: "radar",
+                data: {
+                    labels: [
+                        'HTML',
+                        'CSS',
+                        'Javascript',
+                    ],
+                    datasets: [{
+                        data: [78, 87, 100],
+                        backgroundColor: gradient,
+                    }]
+                },
+                options: { 
+                    
                         plugins: {
                             legend: {
                                 display: false,
@@ -75,8 +59,27 @@ const Profile = () => {
                                 }
                             }
                         }
-                    }}
-                />
+                    },
+            })
+        }
+    }
+
+    const projects = [
+        {name: 'project1', position: {x: 1550, y: 500}, type: 'html', list: [{name: 'Quiz1', marked: 100}, {name: 'Quiz2', marked: 10}]},
+        {name: 'project2', position: {x: 850, y: 700}, type: 'css', list: [{name: 'Quiz1', marked: 100}, {name: 'Quiz2', marked: 10}]},
+        {name: 'project3', position: {x: 1300, y: 1000}, type: 'js', list: [{name: 'Quiz1', marked: 100}, {name: 'Quiz2', marked: 10}]}
+    ]
+
+    return (
+        <Contents>
+            <Skills>
+                <div className="title">SKILLS</div>
+                <div className="description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum turpis sed pretium sodales.
+                </div>
+                <div>
+            <canvas ref={skillChart}></canvas>
+        </div>
             </Skills>
             <hr style={{ width: "100%", padding: '0', margin: '0', border: 'none', height: '1px', backgroundColor: 'black' }} />
             <Skills>
@@ -96,15 +99,11 @@ const Profile = () => {
                     <div className="oval-descp" style={{backgroundColor: "#e64715"}} />
                     <div className="oval-name">Bad</div>
                 </Oval>
-
-                    {/* {projects.map((project, index) => 
-                    console.log(project)
-                        )} */}
-                        <div className="project-lists">
-                        {projects.map((project, index) =>{
-                            return <Projects project={project} key={index}/>;}
-                        )}
-                        </div>
+                <div className="project-lists">
+                {projects.map((project, index) =>{
+                    return <Projects project={project} key={index}/>;}
+                )}
+                </div>
         </Contents>
     );
 };
