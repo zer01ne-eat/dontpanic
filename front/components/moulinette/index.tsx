84d3678b0dd8ed45b091f4398bc87fd672d4c89b
kreativemoon <mnyo@kakao.com>
2021-10-28 18:04:13 +0900

    moulinette icon added in map

---
+++ front/components/moulinette/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MoulinetteContainer } from "./styles";
+
+const Moulinette = () => {
+    return(
+        <MoulinetteContainer>
+            <img src='../../imgs/ai-machine.svg' />
+        </MoulinetteContainer>
+    );
+}
+
+export default Moulinette;
---
+++ front/components/moulinette/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const MoulinetteContainer = styled.div `
+    & img{
+        position: absolute;
+        top: 1000;
+        left: 2000;
+        height: 100px;
+        width: 100px;
+    }
+    `;
